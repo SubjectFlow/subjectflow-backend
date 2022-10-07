5483a1fe389d5c8c19201c36f39e3f35d96dcc3a
Ted Barrett <tlbarrett@student.unimelb.edu.au>
2022-10-07 19:36:26 +1100

    some file system set up

--- app/main.py
+++ app/main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+from app.routers import dbRouter
+
+config = dotenv_values(".env")
 
 
 app = FastAPI()
+
+app.include_router(dbRouter.router)
+
+
+@app.on_event("startup")
+def startup_db_client():
+    print("Connecting to mongo database.")
+    app.mongodb_client = MongoClient(config["MONGO_CONNECTION_STRING"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connection completed.")
+
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
 
 
 @app.get("/")
@@ -12,3 +33,8 @@ async def root():
 @app.get("/favicon.ico")
 async def favicon():
     return None
+
+
+@app.get("/status")
+async def status():
+    return {"progress": "25%"}
---
+++ app/routers/test_dbRouter.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_get_subject():
+    response = client.post("/db/getsubject")
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
