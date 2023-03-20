1bf85dd30538b53c0f8cb5515a8aa2509461ccbf
crispycaterpillar <crispy.caterpillar@gmail.com>
2023-03-20 21:36:32 +1100

    updated basic api

--- subjectflow_backend/models/reqCriteria.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from typing import Optional, List, Tuple
-from pydantic import BaseModel, Field
-from subject import Subject
-
-
-class ReqCriteria(BaseModel):
-    criterion: Tuple[str, Subject]
--- subjectflow_backend/models/subject.py
+++ subjectflow_backend/models/subject.py
@@ -1,10 +1,16 @@
-import uuid
+from __future__ import annotations
 from typing import Optional, List, Tuple
 from pydantic import BaseModel, Field
-from reqCriteria import ReqCriteria
 
 
 class Subject(BaseModel):
     name: str
     code: str
     reqOptions: List[ReqCriteria] = None
+
+
+class ReqCriteria(BaseModel):
+    criterion: Tuple[str, Subject]
+
+
+Subject.update_forward_refs()
