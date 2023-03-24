489e9a9217292e2e1c528f4ae222a42b3702a259
crispycaterpillar <crispy.caterpillar@gmail.com>
2023-03-24 18:36:49 +1100

    added logic util to convert expr to dnf

---
+++ subjectflow_backend/scraper/logic.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from enum import Enum
+from pydantic import BaseModel
+
+
+class LogicOp(Enum):
+    AND = 1
+    OR = 2
+
+
+class Expr(BaseModel):
+    operator: LogicOp
+    operands: tuple[Expr | Literal, Expr | Literal]
+
+
+class Literal(BaseModel):
+    code: bool
+    content: str
+
+
+Expr.update_forward_refs()
+
+
+def toDNF(expr: Expr | Literal) -> list[list[Literal]]:
+    if isinstance(expr, Literal):
+        return [[expr]]
+    elif expr.operator == LogicOp.OR:
+        return toDNF(expr.operands[0]) + toDNF(expr.operands[1])
+    else:
+        res = []
+        for a in toDNF(expr.operands[0]):
+            for b in toDNF(expr.operands[1]):
+                res.append(a + b)
+
+        return res
